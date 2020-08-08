dc1690b16ccd638055c9a6704baca6f6710b791c
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 01:34:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,13 @@ class SessionsController < ApplicationController
     if @user == nil
       redirect_to 'new'
     else
-      return head(:forbidden) unless @user.authenticate(params[:user][:password]) #logs you in with correct password 
+      return head(:forbidden) unless @user.authenticate(params[:user][:password]) #logs you in with correct password
       session[:user_id] = @user.id
       redirect_to homepage_path(@user)
     end
   end
 
 end
+
+#returns the user if credentials match
+#returns falsey if credentials don't match 
