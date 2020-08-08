5b8d213f7fe9206c44dde506e83a61ee68c28fd7
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 01:25:49 +0000

    Automatically backed up by Learn

--- db/003_create_users.rb
+++ db/003_create_users.rb
@@ -1,11 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password
+      t.string :name
+      t.string :password_digest 
 
       t.timestamps null: false
     end
   end
 end
- 5  db/migrate/20180207233223_change_column_name.rb
