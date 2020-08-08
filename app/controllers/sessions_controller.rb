957ef5faa3d5bbb116cd829d82d71c93311ecc65
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 01:32:36 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
 
 # SessionsController#create method l
   def create
-    @user = User.find_by(name: params[:user][:name]) #logs you in withcorrect name 
+    @user = User.find_by(name: params[:user][:name]) #logs you in withcorrect name
     if @user == nil
       redirect_to 'new'
     else
-      return head(:forbidden) unless @user.authenticate(params[:user][:password])
+      return head(:forbidden) unless @user.authenticate(params[:user][:password]) #logs you in with correct password 
       session[:user_id] = @user.id
       redirect_to homepage_path(@user)
     end
