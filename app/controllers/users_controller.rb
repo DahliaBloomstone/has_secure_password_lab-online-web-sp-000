dc4ef19977791131955e290ac6193f4803c86635
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 01:33:00 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
 
+#creates a new user
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
+    if @user.save #saves user
+      session[:user_id] = @user.id #logs you in 
       redirect_to homepage_path(@user)
     else
       redirect_to '/'
