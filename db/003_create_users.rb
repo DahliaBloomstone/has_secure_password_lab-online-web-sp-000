30ca4f76f459584d0aee8acc8967bb9a0b22ecc2
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-08 01:25:07 +0000

    Automatically backed up by Learn

---
+++ db/003_create_users.rb
@@ -0,0 +1,9 @@
+create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+
+end
